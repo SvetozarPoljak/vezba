7d45abf437def859362a01878f566fdb5fac04d0
SvetozarPoljak <svetozarpoljak173@gmail.com>
2024-03-18 22:30:30 +0100

    Calculation function added

--- prvi_git_primer/zadatak1/main.c
+++ prvi_git_primer/zadatak1/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
+int Calculate(int a, int b){
+
+	if(a < b)
+		return b - a;
+	else
+		return a - b;
+
+}
+
 int main()
 {
 	// kod za racunanje apsolutne vrednosti razlike dva broja a i b
 	int a = 3;
 	int b = 8;
-	int c;
-	
-	if(a > b)
-	{
-		c = a - b; 
-	}
-	else
-	{
-		c = b - a;	
-	}
+	int c = Calculate(a, b);
 
 	printf("Rezultat je c = %d", c);
 	return 0;
