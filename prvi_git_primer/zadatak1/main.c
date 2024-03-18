6f1496ce042fb52d4e22d03e704fc171196be2fd
SvetozarPoljak <svetozarpoljak173@gmail.com>
2024-03-18 22:41:48 +0100

    printResult function added

--- prvi_git_primer/zadatak1/main.c
+++ prvi_git_primer/zadatak1/main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 
-int main()
-{
-	// kod za racunanje apsolutne vrednosti razlike dva broja a i b
-	int a = 3;
-	int b = 8;
-	int c;
-	
-	if(a > b)
-	{
-		c = a - b; 
-	}
-	else
-	{
-		c = b - a;	
-	}
+void printResult(int c){
 
 	printf("Rezultat je c = %d", c);
+
+}
+
+int main()
+{
+	// ispis rezultata
+	int c;
+	printResult(c);
+
 	return 0;
 }
